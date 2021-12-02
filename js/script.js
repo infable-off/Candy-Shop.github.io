3c0fbc1b19c5fa5e75098e84556095fd70fb21f6
Vladislav Gusev <73646096+infable-off@users.noreply.github.com>
2021-12-02 22:27:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-    ('.header__burger').click(function (event){
+    document.('.header__burger').click(function (event){
         ('.header__burger, .header__menu').toggleClass('active');
         ('body').toggleClass('lock');
     }
